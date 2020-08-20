de64984190ce4d3ac9aa66f504a993d9eea5eac3
LEMSantos <lucas_m-santos@hotmail.com>
2020-08-20 00:03:19 -0300

    Add - registra o moment como dependência do projeto

--- quasar.conf.js
+++ quasar.conf.js
@@ -21,8 +21,8 @@ module.exports = function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
-
       'axios',
+      'moment',
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
@@ -95,6 +95,7 @@ module.exports = function (/* ctx */) {
 
       // Quasar plugins
       plugins: [],
+      cssAddon: true,
     },
 
     // animations: 'all', // --- includes all animations
