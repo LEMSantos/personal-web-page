a23b9628b7e78c257682f5b83821f9c7483b341b
LEMSantos <lucas_m-santos@hotmail.com>
2020-08-22 18:26:49 -0300

    Add - uso do env

--- quasar.conf.js
+++ quasar.conf.js
@@ -9,6 +9,8 @@
 /* eslint func-names: 0 */
 /* eslint global-require: 0 */
 
+const env = require('quasar-dotenv').config();
+
 module.exports = function (/* ctx */) {
   return {
     // https://quasar.dev/quasar-cli/supporting-ts
@@ -47,6 +49,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+      env,
 
       // transpile: false,
 
